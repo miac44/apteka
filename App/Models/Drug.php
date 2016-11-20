6e7fcc7b351059deff9391eb9ecabf0d078bb39a
bigazzzz <bigazzzz@gmail.com>
2016-11-20 21:12:36 +0300

    Normal Search. First Iteration

--- App/Models/Drug.php
+++ App/Models/Drug.php
@@ -44,11 +44,51 @@ class Drug extends Model
             __CLASS__,
             array('search' => '%' . $search . '%')
         );
+        $bad_words =
+            [
+                'капс.',
+                'упаковка',
+                'п.п.о.',
+                'контурная',
+                'ячейковая',
+                'таб.',
+                ',',
+                'кардио',
+                'шип.',
+                'п/киш-раств.',
+                'раствор./кишечн.',
+                'п.о.',
+                'об.',
+                'пор.',
+                'д/приг.',
+                'назальный',
+                'р-ра'
+            ];
+        $bad_regexp =
+            [
+                '\d+\s?мг',
+                '\d+\s?%',
+                '№\s?\d+',
+                'N\s?\d+',
+                '\d+\s?мл',
+            ];
         $result = array();
         foreach ($res as $v) {
-            $result[] = $v->TOVNAME;
+//            $result[] =  substr($v->TOVNAME,0,strpos(trim($v->TOVNAME),' '));
+            $result_word =$v->TOVNAME;
+            foreach ($bad_words as $word) {
+                $result_word =  str_replace($word, '', $result_word);
+            }
+            foreach ($bad_regexp as $regexp) {
+                $result_word = preg_replace("/" . $regexp . "/i", "", $result_word);
+            }
+            $result_word = trim(preg_replace("/s+/"," ",$result_word));
+            if (!in_array($result_word, $result)){
+                $result[] = $result_word;
+            }
+
         }
-        return json_encode($result, JSON_UNESCAPED_UNICODE);
+        return json_encode(array_unique($result), JSON_UNESCAPED_UNICODE);
 
     }
 

84e072bfe9fd3cebf7ac3199b968136b3379019c
bigazzzz <bigazzzz@gmail.com>
2016-11-22 23:54:58 +0300

    Replaced from bad_regexp to end of string. bad_words & bad_regexp updated

--- App/Models/Drug.php
+++ App/Models/Drug.php
@@ -35,6 +35,10 @@ class Drug extends Model
         return $res;
     }
 
+    /**
+     * @param $search
+     * @return string
+     */
     public function json_search($search)
     {
         $db = \App\Db::instance();
@@ -51,14 +55,14 @@ class Drug extends Model
                 if (strpos($result_word, $word)>0) {
                     $result_word = substr($result_word, 0, strpos($result_word, $word));
                 };
-            }
+            };
             foreach (\App\Config::instance()->bad_regexp as $regexp) {
-                $result_word = preg_replace("/" . $regexp . "/i", "", $result_word);
-            }
+                $result_word = preg_replace("/(.+)" . $regexp . "/i", "$1", $result_word);
+            };
             $result_word = trim(preg_replace("/s+/"," ",$result_word));
             if (!in_array($result_word, $result)){
                 $result[] = $result_word;
-            }
+            };
         }
         return json_encode(array_unique($result), JSON_UNESCAPED_UNICODE);
 
